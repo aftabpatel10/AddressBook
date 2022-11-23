2bc0def57c963d95d34b32c4e4e5ade365a6afe8
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-23 09:06:47 +0530

    Creating a contact in Address book

---
+++ AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Address Book Program!\n");
+        }
+        public static void CreatingContact()
+        {
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your Phone_Number");
+            double phoneNo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your Email_Id");
+            string emailId = Console.ReadLine();
+            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n Phone Number : {phoneNo}\n Email ID : {emailId}");
+        }
+
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            CreatingContact();
+        }
+    }
+}
