fbd1d004153216f8b081b89b44e711b3af7bd88f
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-23 09:19:18 +0530

    Adding Contacts in AddressBook And Showing

---
+++ ContactList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class ContactList
+    {
+        public ContactList()
+        {
+        }
+
+        internal void AddPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ListPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void WelcomeMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
